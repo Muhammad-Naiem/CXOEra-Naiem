40af309caea464da2a0e180f66c0fc8b84bf2dc6
Muhammad-Naiem <80773177+Muhammad-Naiem@users.noreply.github.com>
2022-12-06 20:01:33 +0600

    'section-added-on-home-page'

--- js/common-scripts.js
+++ js/common-scripts.js
@@ -1,22 +1,51 @@
+(function ($) {
+    $(function () {
 
-(function($){
-	$(function(){
+        $('.sub-nav').parent('li').addClass('has-sub-nav')
 
 
-    $('input').each(function () {
-        var $_this = (this)
-        $('input').on('keyup', function () {
-            var $$_this = $(this)
+        if ($('.sponsor-item-wrap').length) {
+            $('.sponsor-item-wrap').slick({
+                speed: 5000,
+                autoplay: true,
+                autoplaySpeed: 0,
+                centerMode: true,
+                swipe: false,
+                cssEase: 'linear',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                variableWidth: true,
+                infinite: true,
+                initialSlide: 1,
+                arrows: false,
+                buttons: false,
+                pauseOnHover: false,
+            });
+        }
 
-            if ($$_this.val() == '') {
-                $$_this.parent().removeClass('focused');
-            } else {
-                $$_this.parent().addClass('focused');
-            }
-        });
+        if ($('.company-updates-sliders').length) {
+            $('.company-updates-sliders').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                prevArrow: '.slider-arrow-left',
+                nextArrow: '.slider-arrow-right',
+            });
+        }
 
-    })
-     
-	})// End ready function.
+        $('input').each(function () {
+            var $_this = (this)
+            $('input').on('keyup', function () {
+                var $$_this = $(this)
+
+                if ($$_this.val() == '') {
+                    $$_this.parent().removeClass('focused');
+                } else {
+                    $$_this.parent().addClass('focused');
+                }
+            });
+
+        })
+
+    }) // End ready function.
 })(jQuery)
-
