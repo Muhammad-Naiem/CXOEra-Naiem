1032a373e248438a46b35352d7d7e28779e49556
Muhammad-Naiem <80773177+Muhammad-Naiem@users.noreply.github.com>
2022-12-08 17:25:57 +0600

    'new-updates'

--- js/common-scripts.js
+++ js/common-scripts.js
@@ -1,7 +1,61 @@
 (function ($) {
     $(function () {
 
+        $('#phone-nav').click(function () {
+            $('body').toggleClass('navShown')
+        })
+
         $('.sub-nav').parent('li').addClass('has-sub-nav')
+        if ($(window).width() > 991) {
+
+$('.header-cta a.login').mouseenter(function(){
+    $(this).addClass('hoverd')
+    $('.main-nav > ul > li').stop(true, true).removeClass('hoverd')
+})
+
+            $('.main-nav > ul > li, .header-cta a.login').addClass('hoverd')
+            $('.main-nav > ul > li > a').mouseenter(function (e) {
+                e.preventDefault()
+                console.log('enter')
+                $('.main-nav > ul > li, .header-cta a.login').stop(true, true).removeClass('hoverd')
+                $(this).parent('li').stop(true, true).addClass('hoverd')
+            })
+            $('.main-nav > ul > li > a, .header-cta a.login').mouseleave(function () {
+                console.log('enter')
+                $('.main-nav > ul > li, .header-cta a.login').stop(true, true).addClass('hoverd')
+            })
+
+            $('.has-sub-nav').each(function () {
+                var $this = $(this)
+                $this.find('> a').mouseenter(function () {
+                    $(this).stop(true, true).addClass('nav-open')
+                    $this.find('.sub-nav').stop(true, true).fadeIn()
+                })
+                $this.mouseleave(function () {
+                    $('.has-sub-nav a').removeClass('nav-open')
+                    $this.find('.sub-nav').stop(true, true).fadeOut()
+                })
+            })
+        }
+
+        if ($(window).width() < 992) {
+            $(".has-sub-nav").each(function () {
+                var $this = $(this);
+                $this.find(" > a").on("click touch", function (e) {
+                    e.preventDefault();
+                    $(".has-sub-nav > a").removeClass("nav-open")
+                    $(".sub-nav").slideUp();
+                    if ($this.find(".sub-nav:visible").length) {
+                        $(".has-sub-nav > a").removeClass("nav-open")
+                        $(".sub-nav").slideUp();
+                    } else {
+                        $this.find('> a').addClass("nav-open")
+                        $(".sub-nav").slideUp();
+                        $this.find(".sub-nav").slideDown();
+                    }
+                })
+            })
+        }
 
 
         if ($('.sponsor-item-wrap').length) {
@@ -19,7 +73,7 @@
                 initialSlide: 1,
                 arrows: false,
                 buttons: false,
-                pauseOnHover: false,
+                pauseOnHover: false
             });
         }
 
@@ -30,6 +84,23 @@
                 slidesToScroll: 1,
                 prevArrow: '.slider-arrow-left',
                 nextArrow: '.slider-arrow-right',
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                            adaptiveHeight: true,
+                        },
+        },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        },
+        },
+      ],
             });
         }
 
