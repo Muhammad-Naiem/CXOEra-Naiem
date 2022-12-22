bec190febf84b50c61165ca4fb06bb7e1ec2943c
Muhammad-Naiem <80773177+Muhammad-Naiem@users.noreply.github.com>
2022-12-22 17:02:47 +0600

    'page-added'

--- js/common-scripts.js
+++ js/common-scripts.js
@@ -117,7 +117,7 @@
             });
 
         })
-        
+
         //faq
         $(".question-content-info").each(function () {
             var $this = $(this);
@@ -145,11 +145,11 @@
             $(activeTab).fadeIn();
             return false;
         });
-        
-        if($('.blog-page').length){
+
+        if ($('.blog-page').length) {
             $('body').addClass('blog-page')
         }
-        if($('.admin-wrap').length){
+        if ($('.admin-wrap').length) {
             $('body').addClass('admin-page')
         }
 
@@ -184,53 +184,74 @@
             });
         }
         
-        
         $('.survey-progress-item').eq(0).addClass('progress-active')
         $('.admin-survey-tab-item:first').show();
         $('.admin-survey-item').each(function () {
             var $this = $(this);
-            $('.next-prev-btns a').click(function () {
-//                if ($this.find('input').is(':checked') && $this.find("textarea").val() != "") {
-                    $('.admin-survey-tab-item').hide();
-                    var activeTab = $(this).attr('href');
-                    $(activeTab).fadeIn(function () {
-                        console.log('text')
-                        $('html, body').animate({
-                            scrollTop: 0
-                        }, '300');
-                    });
-//                }
+            $('.survey-next-prev a').click(function () {
+                //                if ($this.find('input').is(':checked') && $this.find("textarea").val() != "") {
+                $('.admin-survey-tab-item').hide();
+                var activeTab = $(this).attr('href');
+                $(activeTab).fadeIn(function () {
+                    console.log('text')
+                    $('html, body').animate({
+                        scrollTop: 0
+                    }, '300');
+                });
+                //                }
                 return false;
             });
         })
         
+        $('.registration-form-progress-item').eq(0).addClass('registration-form-progress-active')
+        $('.registration-form-tab-item:first').show();
+        $('.registration-form-tab-item').each(function () {
+            var $this = $(this);
+            $('.registration-form-row-input').each(function () {
+                var $$this = $(this)
+                $('.speaker-next-prev a').click(function () {
+//                    if ($$this.find('input').val() != "" && $$this.find("textarea").val() != "") {
+                        $('.registration-form-progress-active').next().addClass('registration-form-progress-active')
+                        $('.registration-form-tab-item').hide();
+                        var activeTab = $(this).attr('href');
+                        $(activeTab).fadeIn();
+//                    }
+                    return false;
+                });
+            })
+        })
         
-//        $('.filter-event-row').each(function(){
-//            var $this = $(this);
-//            var radioC = $(this).find('input[type="radio"]')
-//            var checkboxC = $(this).find('input[type="checkbox"]')
-//            console.log(radioC)
-//            
-////            $this.find('.checkall input').on('change', function(){
-////                var thisChck = $(this)
-////                if(thisChck.prop("checked")){
-////                    checkboxC.attr('checked', true)
-////                }else{
-////                    $('.filter-event-checkbox input[type="checkbox"]').attr('checked', false)
-////                }
-////            })
-//            
-//            $this.find('input[type="radio"]').on('change', function () {
-//                console.log(radioC)
-//                if (radioC.prop("checked")) {
-//                    $('.filter-event-checkbox input[type="checkbox"]').attr('disabled', true)
-//                    $('.filter-event-checkbox input[type="checkbox"]').attr('checked', 'unche')
-//                    checkboxC.attr('disabled', false)
-//                }
-//            })
-//        })
-        
-        
+        $('.speaker-prev a').click(function(){
+            $('.registration-form-progress-item:last-child').removeClass('registration-form-progress-active')
+        })
+
+
+        //        $('.filter-event-row').each(function(){
+        //            var $this = $(this);
+        //            var radioC = $(this).find('input[type="radio"]')
+        //            var checkboxC = $(this).find('input[type="checkbox"]')
+        //            console.log(radioC)
+        //            
+        ////            $this.find('.checkall input').on('change', function(){
+        ////                var thisChck = $(this)
+        ////                if(thisChck.prop("checked")){
+        ////                    checkboxC.attr('checked', true)
+        ////                }else{
+        ////                    $('.filter-event-checkbox input[type="checkbox"]').attr('checked', false)
+        ////                }
+        ////            })
+        //            
+        //            $this.find('input[type="radio"]').on('change', function () {
+        //                console.log(radioC)
+        //                if (radioC.prop("checked")) {
+        //                    $('.filter-event-checkbox input[type="checkbox"]').attr('disabled', true)
+        //                    $('.filter-event-checkbox input[type="checkbox"]').attr('checked', 'unche')
+        //                    checkboxC.attr('disabled', false)
+        //                }
+        //            })
+        //        })
+
+
         $('.filter-event-row').each(function () {
             var $$this = $(this)
             var selectAllItems = $$this.find('.checkall input');
@@ -261,8 +282,8 @@
                 }
             })
         })
-        
-        $('.filter-event-checkbox input').click(function(){
+
+        $('.filter-event-checkbox input').click(function () {
             if (!$(this).prop("checked")) {
                 $(".filter-event-checkbox.checkall input").prop("checked", false);
             }
@@ -290,15 +311,15 @@
 
             });
         })
-        
-        
+
+
         $('.filter-mega-menu-item').each(function () {
             var $this = $(this);
             $('.summits-filter-right ul li a').not('.summits-filter-right ul li.reset-filters a').click(function () {
                 $('.filter-mega-menu-item').hide();
                 $('.summits-filter-right ul li a').removeClass('filter-active');
                 $(this).addClass('filter-active');
-                setTimeout(function(){
+                setTimeout(function () {
                     $('.filter-mega-menu-wrap').slideDown();
                 }, 200)
                 var activeTab = $(this).attr('href');
@@ -306,23 +327,159 @@
                 return false;
             });
         })
-        
-        $('.filter-mega-menu-close').click(function(){
+
+        $('.filter-mega-menu-close').click(function () {
             setTimeout(function () {
                 $('.filter-mega-menu-item').hide();
                 $('.summits-filter-right ul li a').removeClass('filter-active');
             }, 400)
             $('.filter-mega-menu-wrap').slideUp();
         })
-        
-        $('.summits-event-bullet').each(function(){
+
+        $('.summits-event-bullet').each(function () {
             var $$this = $(this);
-            
-            $$this.find('img').click(function(){
+
+            $$this.find('img').click(function () {
                 $$this.remove()
             })
-            
+
         })
+
+        //Summit Detail
+        $('.cxo-summit-tab-trigger ul li').click(function (e) {
+            e.preventDefault();
+            $('.cxo-summit-tab-trigger ul li').removeClass('tab-active');
+            $(this).addClass('tab-active');
+            $('.cxo-summit-item-wrap .cxo-summit-item').hide();
+            var activeTab = $(this).find('a').attr('href');
+            $(activeTab).fadeIn();
+            return false;
+        });
+
+        //Summit Detail
+        $('.summit-agenda-tab-trigger ul li').click(function (e) {
+            e.preventDefault();
+            $('.summit-agenda-tab-trigger ul li').removeClass('tab-active');
+            $(this).addClass('tab-active');
+            $('.summit-agenda-tab-item-wrap .summit-agenda-tab-item').hide();
+            var activeTab = $(this).find('a').attr('href');
+            $(activeTab).fadeIn();
+            return false;
+        });
+
+        //Summit Detail
+        if ($('.cxoera-executive-board-item-wrap').length) {
+            $('.cxoera-executive-board-item-wrap').slick({
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                prevArrow: '.slider-arrow-left',
+                nextArrow: '.slider-arrow-right',
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2,
+                            adaptiveHeight: true,
+                        },
+    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                        },
+    },
+  ],
+            });
+        }
+
+        //Summit Detail
+        if ($("select.styled-select").length) {
+            $("select.styled-select").selectric();
+        }
+
+        //Summit Detail
+        if ($('.summit-speakers-item-wrap').length) {
+            $('.summit-speakers-item-wrap').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                mobileFirst: true,
+                prevArrow: '.slider-arrow-left',
+                nextArrow: '.slider-arrow-right',
+                responsive: [
+                    {
+                        breakpoint: 600,
+                        settings: 'unslick'
+                }
+            ]
+            })
+
+            $(window).on('resize', function () {
+                $('.summit-speakers-item-wrap').slick('resize');
+            });
+        }
+
+        //Summit Detail
+        $('.executive-board-item-btn a').click(function (e) {
+            $('body').addClass('details-popup')
+            e.preventDefault()
+            $(".speaker-detail-wrap").fadeToggle()
+            $('.popup-close').click(function () {
+                $(".speaker-detail-wrap").fadeOut()
+                $('body').removeClass('details-popup')
+            })
+
+        });
+
+
+        if ($(".registration-form-row-input input[type='number']").length) {
+            var countryData = window.intlTelInputGlobals.getCountryData(),
+                input = document.querySelector(".registration-form-row-input input[type='number']");
+
+            var iti = window.intlTelInput(input, {
+                hiddenInput: "full_phone"
+            });
+
+        }
+
+        if ($("select.selectric-select").length) {
+            $("select.selectric-select").selectric();
+        }
+        
+        $('.upload-btn').each(function(){
+            var $$this = $(this);
+            var realFileBtn = $$this.find("input");
+            var customBtn = $$this.find("span");
+            var customImage = $$this.find("figure");
+            
+            customBtn.on("click", function () {
+                realFileBtn.click();
+            });
+            customImage.on("click", function () {
+                realFileBtn.click();
+            });
+
+            
+             realFileBtn.on("change", function () {
+                 var filepath = this.value;
+                 var m = filepath.match(/([^\/\\]+)$/);
+                 var filename = m[1];
+                 customBtn.text(filename);
+             });
+        })
+        
+        $('.event-cancel-trigger').on('click', function(e){
+            e.preventDefault()
+            $('.event-cancel-wrap').fadeIn()
+        }) 
+        $('.event-go-back-btn a').on('click', function(e){
+            e.preventDefault()
+            $('.event-cancel-wrap').fadeOut()
+        })
+        
         
         
 
